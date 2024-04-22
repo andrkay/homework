8f2ae0c72a4e8940aedc724180fb9c623e2ae446
Касиев Андрей <124569763+andrkay@users.noreply.github.com>
2024-04-22 19:18:50 +0300

    Update Z3.php

--- myLabs.ru/4/Z3.php
+++ myLabs.ru/4/Z3.php
@@ -1,8 +1,12 @@
 <?php
+
 $number = 12345;
 
-$firstDigit = (int) $number[0];
-if ($firstDigit >= 1 && $firstDigit <= 3) {
+$number_str = (string) $number;
+
+$first_char = $number_str[0];
+
+if ($first_char == '1' || $first_char == '2' || $first_char == '3') {
     echo "Первая цифра числа - 1, 2 или 3";
 } else {
     echo "Первая цифра числа не является 1, 2 или 3";
