a19f6eac11f32b42efd210127bdec0b923915817
Касиев Андрей <124569763+andrkay@users.noreply.github.com>
2024-04-22 19:09:49 +0300

    Update 6.php

--- myLabs.ru/5/6.php
+++ myLabs.ru/5/6.php
@@ -1,12 +1,5 @@
 <?php
-$host = 'localhost'; // имя хоста
-$user = 'root';      // имя пользователя
-$pass = '';          // пароль
-$name = 'mydb';      // имя базы данных
-
-$link = mysqli_connect($host, $user, $pass, $name);
-
-$query = 'SELECT * FROM users';
-$res = mysqli_query($link, $query) or die(mysqli_error($link));
-var_dump($res);
+for ($i = 1; $i <= 100; $i++) {
+    echo $i . "<br>";
+}
 ?>
